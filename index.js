91945b4f2e0b23c5030f2ef197890b47be649732
Harshit Bhargava <harshitbhargava8057@gmail.com>
2023-10-04 22:55:11 +0530

    upload files

---
+++ index.js
@@ -0,0 +1,37 @@
+function test(){
+    
+    var nm=document.getElementById("name").value;
+    var em=document.getElementById("email").value;
+    var ph=document.getElementById("phone").value;
+    var ad=document.getElementById("address").value;
+    var rl=document.getElementById("dropdown").value;
+    var inp=document.querySelector('input[name="source"]:checked').value;
+    var dom=document.querySelector('input[name="prefer"]:checked').value;
+    var fd=document.getElementById("feedback").value;
+    
+
+// store in local storage
+    var nm=localStorage.setItem("nm",nm);
+    var em=localStorage.setItem("em",em);
+    var ph=localStorage.setItem("ph",ph);
+    var ad=localStorage.setItem("ad",ad);
+    var rl=localStorage.setItem("rl",rl);
+    var inp=localStorage.setItem("inp",inp);
+    var dom=localStorage.setItem("dom",dom);
+    var fd=localStorage.setItem("fd",fd);
+
+
+
+
+
+const form = document.querySelector("form");
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    const captchaResponse=grecaptcha.getResponse();
+
+    if(!captchaResponse.length>0){
+        throw new Error(alert("Captcha not Valid"));
+    }});
+}
+    
